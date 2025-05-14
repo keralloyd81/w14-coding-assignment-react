2dcf43bc7b019377c7a455352ce03081adb496d3
Kera Lloyd <lloykera@gmail.com>
2025-05-14 11:34:40 -0600

    updated App.tsx to show removed error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-//import React from 'react';  --removed due to TS error
+//import React from 'react';  --removed due to TS error but left to show
 import Header from './components/Header';
 import RecipeList from './components/RecipeList';
 import recipes from './data/recipes';
